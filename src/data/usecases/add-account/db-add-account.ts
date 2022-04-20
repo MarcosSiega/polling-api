c59459ab52684bf0ba8aac247df1ef858f57779f
Marcos Siega <marcosmsiega@gmail.com>
2022-04-20 18:04:57 -0300

    feat: ensure AddAccountRepository adds a valid account

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,19 +1,24 @@
+import { AddAccountRepository } from 'data/protocols/add-account-repository'
 import { Encrypter, AccountModel, AddAccount, AddAccountModel } from './db-add-account-protocols'
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
-  constructor (encrypter: Encrypter) {
+  private readonly addAccountRepository: AddAccountRepository
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
     this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
   }
 
   async add (account: AddAccountModel): Promise<AccountModel> {
     const newAccount = {
-      id: 'id',
-      name: 'name',
-      email: 'email',
+      id: 'valid_id',
+      name: account.name,
+      email: account.email,
       password: 'valid_password'
     }
-    await this.encrypter.encrypt(account.password)
+    const encryptedString = await this.encrypter.encrypt(account.password)
+    newAccount.password = encryptedString
+    await this.addAccountRepository.add(newAccount)
     return await new Promise(resolve => resolve(newAccount))
   }
 }
